01ad9b22f106aff7d425a55c3a79319bf4f48a50
nrshakib <nrshakib007@gmail.com>
2025-08-23 14:56:46 +0600

    Integrated Sign In Api

---
+++ src/Redux/api/authApi.js
@@ -0,0 +1,68 @@
+import { baseApi } from "../baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    signIn: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth/admin-login",
+        method: "POST",
+        body: credentials,
+      }),
+      invalidatesTags: ["user"],
+    }),
+    ForgetPassword: builder.mutation({
+      query: (data) => {
+        const token = sessionStorage.getItem("accessToken");
+        console.log("Forget Pass Mail Token", token);
+        return {
+          url: "/auth/forget-password",
+          method: "POST",
+          body: data,
+          headers: {
+            "content-type": "application/json",
+          },
+        };
+      },
+      invalidatesTags: ["user"],
+    }),
+    VerifyOtp: builder.mutation({
+      query: (data) => {
+        const token = localStorage.getItem("otpToken");
+        console.log("vetifyOtpToken", token);
+        return {
+          url: "/auth/verify-email",
+          method: "post",
+          body: data,
+          headers: {
+            "content-type": "application/json",
+            token: token,
+          },
+        };
+      },
+      invalidatesTags: ["user"],
+    }),
+    ResetPassword: builder.mutation({
+      query: (data) => {
+        const token = localStorage.getItem("verifiedOtpToken");
+        console.log({ token });
+        return {
+          url: "/auth/reset-password",
+          method: "post",
+          body: data,
+          headers: {
+            // "content-type": "application/json",
+            token: token,
+          },
+        };
+      },
+      invalidatesTags: ["user"],
+    }),
+  }),
+});
+
+export const {
+  useSignInMutation,
+  useForgetPasswordMutation,
+  useVerifyOtpMutation,
+  useResetPasswordMutation,
+} = authApi;
