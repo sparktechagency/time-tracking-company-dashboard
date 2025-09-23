793b212aa0dd87b2942d31965d28906ce34afe97
nrshakib <nrshakib007@gmail.com>
2025-09-23 15:03:56 +0600

    Changed BaseUrl to live

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,9 +1,9 @@
 export const getBaseUrl = () => {
-  return "http://10.10.7.26:5002/api/v1";
-  // return "http://88.198.112.109:5000/api/v1";
+  // return "http://10.10.7.26:5002/api/v1";
+  return "http://88.198.112.109:5000/api/v1";
 };
 
 export const getImageUrl = () => {
-  // return "http://88.198.112.109:5000";
-  return "http://10.10.7.26:5002";
+  return "http://88.198.112.109:5000";
+  // return "http://10.10.7.26:5002";
 };
